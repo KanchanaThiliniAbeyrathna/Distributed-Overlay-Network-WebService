55a7d063dcf2bb2a40a40aff43150e9f8888e2f7
KanchanaMalsha <malshathilini10.13@cse.mrt.ac.lk>
2017-11-05 23:48:24 +0530

    update README.md

--- README.md
+++ README.md
@@ -6,7 +6,11 @@ Develop a simple overlay-based solution using web services that allows a set of 
 ## Run
 Open project in Eclipse IDE
 
-Add Tomcat to your IDE and Run on Server
+Add Tomcat to your IDE
+
+Right click on the project and then select Run As --> Run on Server
+
+API is available at http://localhost:8080/Distributed-Overlay-Network-WebService/
 
 ## Group Members
 130001H A. H. K. T. Aberathne
--- src/main/java/controller/WebServiceController.java
+++ src/main/java/controller/WebServiceController.java
@@ -1,7 +1,12 @@
 package controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import db.NetworkService;
@@ -11,11 +16,15 @@ import db.NetworkService;
  */
 
 @RestController
-@RequestMapping("distibutedsearch")
+@RequestMapping("")
 public class WebServiceController {
-	
+
 	@Autowired
 	private NetworkService net;
-	
-	
+
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<String> getAll() {
+		return new ResponseEntity<String>("working", HttpStatus.OK);
+	}
+
 }
